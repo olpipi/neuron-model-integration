7307c3386c3ca8eb14189fb3b069ac2b57caf63e
olpipi <rudzakevich@ninodom.ru>
2017-04-08 23:08:33 +0300

    Add files via upload

---
+++ Runge_Kutta.h
@@ -0,0 +1,12 @@
+#ifndef __Runge_Kutta__
+#define __Runge_Kutta__
+
+#define N 4 
+
+typedef double vector[N];
+typedef double (*funcdef)(double x, vector y);
+typedef funcdef funcarr[N];
+
+void Runge_Kutta_fourth_order(double *Yn, double Xn, double step, funcarr func);
+
+#endif
